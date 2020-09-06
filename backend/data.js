5c98fe9db1a262497da23e06e0548dd4f8c34206
ffthkrdg <ffthkrdg@gmail.com>
2020-09-06 06:33:24 +0300

    api created

---
+++ backend/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    url:
+      'https://v16m.tiktokcdn.com/9ea67db564d693d42c3099bc49543e5c/5f548b40/video/tos/alisg/tos-alisg-pve-0037/5ae642e387454fcd82ac7d771ef9c2c1/?a=1233&br=3574&bt=1787&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202009060109360101902192241C473760&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=anBza25rZXY8dzMzaDczM0ApZmQ0Z2RlaTtoN2g0ODk8Omc0M2dfMGctcjZfLS01MTRzc15eYjVjMjI2MC81NjQ2NDA6Yw%3D%3D&vl=&vr=',
+    channel: 'sssanga',
+    song: 'blahh hellow 00 prpolleme',
+    likes: '111',
+    messages: '333',
+    description: 'tictoc colone',
+    shares: '222'
+  },
+  {
+    url:
+      'https://v77.tiktokcdn.com/5e33ad8b62146208e903f8a3d1124837/5f5495b7/video/tos/useast2a/tos-useast2a-pve-0068/484f74cd539f493b9fad9bb57467c017/?a=1233&br=4152&bt=2076&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20200906015422010190186142374A01CB&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=MzR5NTw4aTR0djMzZTczM0ApODY4ZDk3NGUzN2Q7Mzw6OGc0czI2Z2UtYXFfLS1iMTZzcy40XjBiLWAwL2I0Nl8uLV46Yw%3D%3D&vl=&vr=',
+    channel: 'hellowkit',
+    song: 'injcet prooperlyt topmege',
+    likes: '444',
+    messages: '555',
+    description: 'rellay imperseinva onp',
+    shares: '666'
+  }
+]
+
+export default data
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,62 @@
 import express from 'express'
 import mongoose from 'mongoose'
-
+import Data from './data.js'
+import Videos from './dbModel.js'
+//app config
 const app = express()
 const port = 9000
 
-app.get('/', (req, res) => res.status(200).send('hello world'))
+//middlewares
+app.use(express.json())
+app.use((req, res, next) => {
+  res.setHeaders('Access-Control-Allow-Origin', '*'),
+    res.setHeaders('Access-Control-Allow-Headers', '*'),
+    next()
+})
 
-app.listen(port, () => console.log(`listening on http://localhost:${port}`))
+//DB config
+const connection_url =
+  'mongodb+srv://admin:cWojsBYb9KoJeJnj@cluster0.0l73i.mongodb.net/tiktok?retryWrites=true&w=majority'
+
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+
+//api endpoints
+app.get('/', (req, res) => {
+  res.send('Hello Worlds!')
+})
+
+app.get('/v1/posts', (req, res) => {
+  res.status(200).send(Data)
+})
+
+app.get('/v2/posts', (req, res) => {
+  Videos.find((err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(200).send(data)
+    }
+  })
+})
+
+app.post('/v2/posts', (req, res) => {
+  //post request is to add data to the database
+  const dbVideos = req.body
+
+  Videos.create(dbVideos, (err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(201).send(data)
+    }
+  })
+})
+
+//listen
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`)
+})
